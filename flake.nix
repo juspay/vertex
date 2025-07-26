395ffe52f3cb3b5356af450277420e3ffa68201e
Sridhar Ratnakumar <srid@srid.ca>
2025-07-26 11:13:32 -0400

    Use nix LSP

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      perSystem = { pkgs, ... }: {
         formatter = pkgs.nixpkgs-fmt;
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
