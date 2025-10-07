3f473ef210fd37ba0ceb5352161be727c0822f15
Sridhar Ratnakumar <srid@srid.ca>
2025-10-07 18:09:19 -0400

    ..

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
   };
 
   inputs = {
-    # Temporarily pinned to specific commit
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
